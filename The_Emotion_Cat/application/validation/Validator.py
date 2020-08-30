a132d3bfe932fd2445dc407664c18d9e516bb6db
anamarda <ana.marda@yahoo.com>
2020-08-30 11:04:50 +0300

    gui validation

--- The_Emotion_Cat/application/validation/Validator.py
+++ The_Emotion_Cat/application/validation/Validator.py
@@ -1,9 +1,57 @@
+class GuiException(Exception):
+    def __init__(self, _message):
+        '''
+        Constructor.
+        
+        Paramaters
+        -----------
+            - _message: string, the error message
+        '''
+        self.message = _message
+    
+    def get_message(self):
+        '''
+        Getter of the message field.
+        
+        Return
+        -------
+            - string, the error message
+        '''
+        return self.message
+
 class Validator():
     def __init__(self):
         pass
     
     def validate_name(self, name):
-        pass
+        '''
+        String validation.
+        
+        Paramaters
+        -----------
+            - name: string
+        
+        Raise
+        ------
+            - GuiException, if the name is None or the null string
+        '''
+        if name == "" or name == None:
+            raise GuiException("Invalid name.")
         
     def validate_number(self, number):
-        pass
+        '''
+        Float nuber validation.
+        
+        Paramaters
+        -----------
+            - number: string
+        
+        Raise
+        ------
+            - GuiException, if the number can not be converted into 
+                float. 
+        '''
+        try:
+            float(number)
+        except Exception:
+            raise GuiException("Invalid number.")
